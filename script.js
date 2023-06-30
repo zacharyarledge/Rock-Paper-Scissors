7abb0190e2963c4c42ee45c3cf0c6896d110b3de
Zachary Arledge <111612740+zacharyarledge@users.noreply.github.com>
2023-06-30 19:02:13 -0400

    Create playRound function

--- script.js
+++ script.js
@@ -10,9 +10,18 @@ function getComputerChoice() {
 }
 
 function playRound(pS, cS) {
+    if (pS == cS) {
+        return console.log("TIE");
+    } else if ((pS == "ROCK" && cS == "PAPER") 
+            || (pS == "PAPER" && cS == "SCISSORS") 
+            || (pS == "SCISSORS" && cS == "ROCK")) {
+        return console.log("LOSE");
+    } else {
+        return console.log("WIN");
+    }
+    }
 
 
-}
 
 let text1 = prompt("ROCK, PAPER, or SCISSORS");
 let text2 = text1.trim();
