88c42a354e69326cb112d0fc3b500324951903ce
Zachary Arledge <111612740+zacharyarledge@users.noreply.github.com>
2023-06-30 19:25:02 -0400

    Create game function

--- script.js
+++ script.js
@@ -9,25 +9,36 @@ function getComputerChoice() {
     }    
 }
 
-function playRound(pS, cS) {
-    if (pS == cS) {
-        return console.log("TIE");
-    } else if ((pS == "ROCK" && cS == "PAPER") 
-            || (pS == "PAPER" && cS == "SCISSORS") 
-            || (pS == "SCISSORS" && cS == "ROCK")) {
-        return console.log("LOSE");
+function playRound() {
+    let text1 = prompt("ROCK, PAPER, or SCISSORS");
+    let text2 = text1.trim();
+    let playerSelection = text2.toUpperCase();
+
+    let computerSelection = getComputerChoice();
+
+    if (playerSelection == computerSelection) {
+        return "TIE";
+    } else if ((playerSelection == "ROCK" && computerSelection == "PAPER") 
+            || (playerSelection == "PAPER" && computerSelection == "SCISSORS") 
+            || (playerSelection == "SCISSORS" && computerSelection == "ROCK")) {
+        return "LOSE";
     } else {
-        return console.log("WIN");
+        return "WIN";
     }
     }
 
+function game() {
+    let game1 = playRound();
+    let game2 = playRound();
+    let game3 = playRound();
+    let game4 = playRound();
+    let game5 = playRound();
+    return console.log(game1, game2, game3, game4, game5)
+}
 
 
-let text1 = prompt("ROCK, PAPER, or SCISSORS");
-let text2 = text1.trim();
-let playerSelection = text2.toUpperCase();
 
-let computerSelection = getComputerChoice();
 
-playRound(playerSelection, computerSelection)
+
+game()
 
